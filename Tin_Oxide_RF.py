4059b6bd4a8a90937da8a301cc6b6fb9bb459006
Kinetic-Drive <37971265+Kinetic-Drive@users.noreply.github.com>
2022-01-24 02:13:55 -0500

    Update Tin_Oxide_RF.py

--- Tin_Oxide_RF.py
+++ Tin_Oxide_RF.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import Tin_Oxide_Data_Processing as d
 from sklearn.ensemble import RandomForestRegressor as rfg
 import csv
@@ -20,22 +19,27 @@ dp.Drop_Bad_Pixels("Average")
 dp.Sort_By_Device()
 dp.Parameter_Encoder()
 train_input, test_input, train_output, test_output = dp.Generate_Sets(.8)
+
 #apply random forest regression to training set
 regr = rfg(random_state=randrange(0, 100))
 regr.fit(train_input, train_output)
+
 #predict output values
 output_predicted = regr.predict(test_input)
 print(pd.DataFrame([output_predicted, test_output]).transpose())
+
 #score the model on the test sets
 r_square = metrics.r2_score(test_output, output_predicted)
 print(r_square)
 print(train_input)
-#visualize rfr
+
+#visualize random forest regression model
 x_val = np.arange(min(train_input), max(train_input), .01)
 x_val = x_val.reshape((len(x_val), 1))
+
 #scatter plot for training data
 plt.scatter(train_input, train_output, color = 'red')
+
 #scatter plot for predicted data
 plt.plot(x_val, output_predicted, color = 'blue')
-
 
